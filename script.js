ddbd4efb20aaec08e7c47ed870576e49372ef138
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-01 18:59:06 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function secondLargest(arr){
+ arr.sort((a,b)=>b-a);
+  return arr[1]
+   
+   
+
+
+}
+
+console.log(secondLargest([23, 66, 50, 12, 6])) 
+console.log(secondLargest([65, 34, 100, 22, 85])) 
+console.log(secondLargest([67, 22, 19, 12, 80]))
